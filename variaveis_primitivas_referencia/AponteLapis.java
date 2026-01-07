d27ced80f3b34c5adc4b9abfc7c88ba55d3c2aab
Pedro <pedro@tridata.com.br>
2026-01-07 16:17:00 -0300

    Cap. 3 - Aponte o lápis - Tipos incompatíveis

---
+++ variaveis_primitivas_referencia/AponteLapis.java
@@ -0,0 +1,17 @@
+class AponteLapis {
+    public static void main(String args[]){
+        // exercício: as comentadas não vão compilar (incompatible types)
+        //int x = 34.5;
+        //boolean boo = x;
+        int g = 17;
+        int y = g;
+        y = y + 10;
+        short s;
+        //s = y;
+        byte b = 3;
+        byte v = b;
+        short n = 12;
+        //v = n;
+        //byte k = 128;
+    }
+}
