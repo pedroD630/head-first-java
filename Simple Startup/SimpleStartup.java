0624ca58cafac819fa2f8ceca3be799d14dc4cd9
Pedro <pedro@tridata.com.br>
2026-01-30 09:04:41 -0300

    feat: implementar método check Yourself

--- Simple Startup/SimpleStartup.java
+++ Simple Startup/SimpleStartup.java
@@ -2,4 +2,19 @@ public class SimpleStartup {
     private int[] locationCells;
     private int numOfHits = 0;
 
+    public String checkYourself(int userGuess){
+        for(int i = 0; i < locationCells.length; i + 1){
+            if(userGuess == locationCells[i]){
+                numOfHits = numOfHits + 1;
+
+                if(numOfHits == 3){
+                    return "kill";
+                } else {
+                    return "hit";
+                }
+            } else {
+                return "miss";
+            }
+        }
+    }
 }
