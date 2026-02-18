dc5132013f7e64f34e96e1c5b454669e3a06e040
Pedro Aspire <pedrildojw@gmail.com>
2026-02-18 16:55:35 -0300

    refactor: Verificar se o palpite do user está no arrayList e retornar kill, hit ou miss

--- Simple Startup/SimpleStartup.java
+++ Simple Startup/SimpleStartup.java
@@ -2,13 +2,21 @@ import java.util.ArrayList;
 
 public class SimpleStartup {
     private ArrayList<String> locationCells;
-    private int numOfHits = 0;
 
     public String checkYourself(String userInput){
 
         String result = "miss";
-
         int index = locationCells.indexOf(userInput);
+
+        if(index >= 0) {
+            locationCells.remove(index);
+
+            if(locationCells.isEmpty()){
+                result = "kill";
+            } else {
+                result = "hit";
+            }
+        }
         return result;
     }
 
