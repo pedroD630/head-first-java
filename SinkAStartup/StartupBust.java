eb953c530f839d89e99586c7daea840bc6cd74aa
Pedro Aspire <pedrildojw@gmail.com>
2026-02-24 16:02:47 -0300

    feat: implementar método de setup do game o método cria as instancias da classe Startup e as adiciona à Array List de startups. Em seguida define os dados básicos de nome e posição para cada

--- SinkAStartup/StartupBust.java
+++ SinkAStartup/StartupBust.java
@@ -1,10 +1,33 @@
 import java.util.ArrayList;
 
 public class StartupBust {
-    public static void main(String[] args) {
-        int numOfGuesses = 0;
 
-        GameHelper helper = new GameHelper();
+    int numOfGuesses = 0;
+    GameHelper helper = new GameHelper();
+    ArrayList<Startup> startups = new ArrayList<>();
+
+    public void setUpGame(){
+        Startup one = new Startup();
+        one.setName("poniez");
+        Startup two = new Startup();
+        two.setName("hacqi");
+        Startup three = new Startup();
+        three.setName("cabista");
+        startups.add(one);
+        startups.add(two);
+        startups.add(three);
+
+        System.out.println("Your goal is to sink three startups.");
+        System.out.println("poniez, hacqi, cabista");
+        System.out.println("Try to sink them all in the fewest number of guesses");
+
+        for (Startup startup : startups) {
+            ArrayList<String> newLocation = helper.placeStartup(3);
+            startup.setLocationCells(newLocation);
+        }
+    }
+    /*public static void main(String[] args) {
+        
         Startup theStartup = new Startup();
 
         // declara randomNum e utiliza (int) para fazer um cast, converter double (retorno) para int, cortando parte fracionaria
@@ -31,5 +54,5 @@ public class StartupBust {
                 break;
             }
         }
-    } 
+    }*/
 }
